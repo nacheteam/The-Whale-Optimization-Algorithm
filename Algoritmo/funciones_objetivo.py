81ce9474d9268bd9cf2fceef775db852d4b607fd
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-05-09 23:26:07 +0200

    Quitados los global y añadida la función 5

--- Algoritmo/funciones_objetivo.py
+++ Algoritmo/funciones_objetivo.py
@@ -1,8 +1,10 @@
 import numpy as np
+import scipy
 
 MIN_BOUND = -100
 MAX_BOUND = 100
 D=10
+e = 2.718281
 
 def setDimension(dim):
     global D
@@ -18,20 +20,20 @@ def getMaxBound():
 
 #High Conditioned Elliptic Function
 def f1(x):
-    global D
     return np.sum(np.float_power(np.repeat(1000000,D),(range(1,D+1)-np.ones(D))/(np.repeat(D,D)-np.ones(D))) * (x*x))
 
 #Bent Cigar Function
 def f2(x):
-    global D
     return np.sum(np.append(1,np.repeat(1000000,D-1))*x*x)
 
 #Discus Function
 def f3(x):
-    global D
     return np.sum(np.append(1000000,np.repeat(1,D-1))*x*x)
 
 #Rosenbrock's Function
 def f4(x):
-    global D
     return np.sum(100*(x[:-1]*x[:-1] - x[1:]*x[1:]) + (x[:-1]-np.ones(D-1))*(x[:-1]-np.ones(D-1)))
+
+#Ackley's Function
+def f5(x):
+    return -20*np.exp(-0.2*np.sqrt((1/D)*np.sum(x*x))) - np.exp((1/D)*np.sum(np.cos(2*scipy.pi*x))) + 20 + e
