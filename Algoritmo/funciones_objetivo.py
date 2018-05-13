15bc03732de027f3af4521b6f2d752d39b407986
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-05-13 23:48:18 +0200

    Función de lectura de matrices y optimos, ahora solo se lee una vez

--- Algoritmo/funciones_objetivo.py
+++ Algoritmo/funciones_objetivo.py
@@ -8,6 +8,9 @@ import scipy
 MIN_BOUND = -100
 MAX_BOUND = 100
 D=10
+num_funciones = 16
+matrices = []
+optimos = []
 
 #Para la función 5
 e = 2.718281
@@ -33,6 +36,15 @@ def getMaxBound():
     global MAX_BOUND
     return MAX_BOUND
 
+#Hay que ejecutarla antes de utilizar ninguna función
+def obtenerOptimosMatrices():
+    global matrices
+    global optimos
+
+    #Lee las matrices y optimos desde 1 hasta num_funciones
+    matrices = [matrices(i) for i in range(1,num_funciones+1)]
+    optimos = [optimos(i) for i in range(1,num_funciones+1)]
+
 ################################################################################
 ##                           FUNCIONES AUXILIARES                             ##
 ################################################################################
@@ -49,7 +61,7 @@ def auxg(x,y):
     return 0.5 + ( np.float_power(np.sin(np.sqrt(x*x+y*y)),2) - 0.5 ) / ( (1+0.001*(x*x+y*y))*(1+0.001*(x*x+y*y)) )
 
 ## Auxiliar de las funciones híbridas
-def leeMatriz(num_func):
+def matrices(num_func):
     f = open("./matrices_optimos/M_{0}_D{1}.txt".format(num_func,D),"r")
     matriz = []
     for line in f:
@@ -60,7 +72,7 @@ def leeMatriz(num_func):
 
     return np.array(matriz)
 
-def leeOptimos(num_func):
+def optimos(num_func):
     f = open("./matrices_optimos/shift_data_{0}.txt".format(num_func),"r")
     optimos = list(filter(None,f.read()[:-1].split(" ")))
     for i in range(len(optimos)):
@@ -170,64 +182,64 @@ def Basicaf14(x,D=D):
 
 #Rotated High Conditioned Elliptic Function
 def f1(x):
-    return Basicaf1(leeMatriz(1).dot(x-leeOptimos(1))) + 100
+    return Basicaf1(matrices[0].dot(x-optimos[0])) + 100
 
 #Rotated Bent Cigar Function
 def f2(x):
-    return Basicaf2(leeMatriz(2).dot(x-leeOptimos(2))) + 200
+    return Basicaf2(matrices[1].dot(x-optimos[1])) + 200
 
 #Rotated Discus Function
 def f3(x):
-    return Basicaf3(leeMatriz(3).dot(x-leeOptimos(3))) + 300
+    return Basicaf3(matrices[2].dot(x-optimos[2])) + 300
 
 #Shifted and Rotated Rosenbrock's Function
 def f4(x):
-    return Basicaf4(leeMatriz(4).dot( np.divide((x-leeOptimos(4))*2.048,100) + np.ones(D))) + 400
+    return Basicaf4(matrices[3].dot( np.divide((x-optimos[3])*2.048,100) + np.ones(D))) + 400
 
 #Shifted and Rotated Ackley's Function
 def f5(x):
-    return Basicaf5(leeMatriz(5).dot(x-leeOptimos(5))) + 800
+    return Basicaf5(matrices[4].dot(x-optimos[4])) + 800
 
 #Shifted and Rotated Weierstrass Function
 def f6(x):
-    return Basicaf6(leeMatriz(6).dot( np.divide(0.5*(x-leeOptimos(6)),100) )) + 600
+    return Basicaf6(matrices[5].dot( np.divide(0.5*(x-optimos[5]),100) )) + 600
 
 #Shifted and Rotated Griewank's Function
 def f7(x):
-    return Basicaf7(leeMatriz(7).dot( np.divide(600*(x-leeOptimos(7)),100) )) + 700
+    return Basicaf7(matrices[6].dot( np.divide(600*(x-optimos[6]),100) )) + 700
 
 #Shifted Rastrigin's Function
 def f8(x):
-    return Basicaf8(np.divide(5.12*(x-leeOptimos(8)),100))+700
+    return Basicaf8(np.divide(5.12*(x-optimos[7]),100))+700
 
 #Shifted and Rotated Rastrigin's Function
 def f9(x):
-    return Basicaf8(leeMatriz(9).dot(np.divide(5.12*(x-leeOptimos(9)),100))) + 900
+    return Basicaf8(matrices[8].dot(np.divide(5.12*(x-optimos[8]),100))) + 900
 
 #Shifted Schwefel's Function
 def f10(x):
-    return Basicaf9(np.divide(1000*(x-leeOptimos(10)),100)) + 1000
+    return Basicaf9(np.divide(1000*(x-optimos[9]),100)) + 1000
 
 #Shifted and Rotated Schwefel's Function
 def f11(x):
-    return Basicaf9(leeMatriz(11).dot(np.divide(1000*(x-leeOptimos(11)),100)))+1100
+    return Basicaf9(matrices[10].dot(np.divide(1000*(x-optimos[10]),100)))+1100
 
 #Shifted and Rotated Katsuura Function
 def f12(x):
-    return Basicaf10(leeMatriz(12).dot(np.divide(5*(x-leeOptimos(12)),100))) + 1200
+    return Basicaf10(matrices[11].dot(np.divide(5*(x-optimos[11]),100))) + 1200
 
 #Shifted and Rotated HappyCat Function
 def f13(x):
-    return Basicaf11(leeMatriz(13).dot(np.divide(5*(x-leeOptimos(13)),100))) + 1300
+    return Basicaf11(matrices[12].dot(np.divide(5*(x-optimos[12]),100))) + 1300
 
 #Shifted and Rotated HGBat Function
 def f14(x):
-    return Basicaf12(leeMatriz(13).dot(np.divide(5*(x-leeOptimos(14)),100)))+1400
+    return Basicaf12(matrices[13].dot(np.divide(5*(x-optimos[13]),100)))+1400
 
 #Shifted and Rotated Expanded Griewank's plus Rosenbrock's Function
 def f15(x):
-    return Basicaf13(leeMatriz(15).dot(np.divide(5*(x-leeOptimos(15)),100)))+1500
+    return Basicaf13(matrices[14].dot(np.divide(5*(x-optimos[14]),100)))+1500
 
 #Shifted and Rotated Expanded Scaffer's F6 Function
 def f16(x):
-    return Basicaf14(leeMatriz(16).dot(x-leeOptimos(16))+np.ones(D)) + 1600
+    return Basicaf14(matrices[15].dot(x-optimos[15])+np.ones(D)) + 1600
