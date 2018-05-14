f56a0e06f4b1c21b0a0ce9b6e601a018b9beae16
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-05-14 16:46:53 +0200

    Mejor formato

--- Algoritmo/resultados.py
+++ Algoritmo/resultados.py
@@ -7,8 +7,12 @@ funciones_objetivo.obtenerOptimosMatrices()
 funciones = funciones_objetivo.getFunciones()
 for dim in [10,30,50,100]:
     funciones_objetivo.setDimension(dim)
+    funciones_objetivo.obtenerOptimosMatrices()
     print("DIMENSION: " + str(dim))
-    fichero.write("DIMENSION: " + str(dim) + "\n")
+    fichero.write("################################################\n")
+    fichero.write("                    DIMENSION " + str(dim) + "\n")
+    fichero.write("################################################\n")
+    fichero.write("\n")
     contador = 1
     for f in funciones:
         solucion,fitness = ballena.Ballena(f,funciones_objetivo.MIN_BOUND,funciones_objetivo.MAX_BOUND,dim)
@@ -18,6 +22,8 @@ for dim in [10,30,50,100]:
         fichero.write("Función número " + str(contador) + "\n")
         fichero.write("Solucion: " + str(solucion) + "\n")
         fichero.write("Fitness: " + str(fitness) + "\n")
+        fichero.write("\n")
         contador+=1
+    fichero.write("\n\n\n")
 
 fichero.close()
