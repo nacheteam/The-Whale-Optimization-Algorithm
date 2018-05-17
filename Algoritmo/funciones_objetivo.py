54270f04bceb46fda313c2635c86994349f462fe
Pablo <pbaeyens31@gmail.com>
2018-05-17 12:20:25 +0200

    Arregla funciones objetivo ups

--- Algoritmo/funciones_objetivo.py
+++ Algoritmo/funciones_objetivo.py
@@ -4,16 +4,16 @@ def getFunciones():
   return [f1, f2, f3, f4, f5] #, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16]
 
 def f1(x):
-  return cec2014(x,1)
+  return cec2014.cec14(x,1)
 
 def f2(x):
-  return cec2014(x,2)
+  return cec2014.cec14(x,2)
 
 def f3(x):
-  return cec2014(x,3)
+  return cec2014.cec14(x,3)
 
 def f4(x):
-  return cec2014(x,4)
+  return cec2014.cec14(x,4)
 
 def f5(x):
-  return cec2014(x,5)
+  return cec2014.cec14(x,5)
