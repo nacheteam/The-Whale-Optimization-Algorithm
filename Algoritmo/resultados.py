17df4ab99065d481cd0a3364c75039fbf19b037a
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-05-17 13:17:43 +0200

    Cambiado el formato con las nuevas funciones y con todas las versiones

--- Algoritmo/resultados.py
+++ Algoritmo/resultados.py
@@ -1,29 +1,33 @@
 import funciones_objetivo
 import ballena
 
-fichero = open("./resultados/v1/resultados.txt","w")
+ballena.setSeed(123456789)
+ballenas = ballena.getFuncionesBallena()
 
-funciones_objetivo.obtenerOptimosMatrices()
-funciones = funciones_objetivo.getFunciones()
-for dim in [10,30,50,100]:
-    funciones_objetivo.setDimension(dim)
-    funciones_objetivo.obtenerOptimosMatrices()
-    print("DIMENSION: " + str(dim))
-    fichero.write("################################################\n")
-    fichero.write("                    DIMENSION " + str(dim) + "\n")
-    fichero.write("################################################\n")
-    fichero.write("\n")
-    contador = 1
-    for f in funciones:
-        solucion,fitness = ballena.Ballena(f,funciones_objetivo.MIN_BOUND,funciones_objetivo.MAX_BOUND,dim)
-        print("Función número " + str(contador))
-        print("Solucion: " + str(solucion))
-        print("Fitness: " + str(fitness))
-        fichero.write("Función número " + str(contador) + "\n")
-        fichero.write("Solucion: " + str(solucion) + "\n")
-        fichero.write("Fitness: " + str(fitness) + "\n")
+for i in range(len(ballenas)):
+
+    print("Versión de la ballena " + str(i+1))
+
+    fichero = open("./resultados/v{0}/resultados.txt".format(i+1),"w")
+
+    funciones = funciones_objetivo.getFunciones()
+    for dim in [10]: #,30,50,100]:
+        print("DIMENSION: " + str(dim))
+        fichero.write("################################################\n")
+        fichero.write("                    DIMENSION " + str(dim) + "\n")
+        fichero.write("################################################\n")
         fichero.write("\n")
-        contador+=1
-    fichero.write("\n\n\n")
+        contador = 1
+        for f in funciones:
+            solucion,fitness = ballenas[i](f,-100,100,dim)
+            print("Función número " + str(contador))
+            print("Solucion: " + str(solucion))
+            print("Fitness: " + str(fitness))
+            fichero.write("Función número " + str(contador) + "\n")
+            fichero.write("Solucion: " + str(solucion) + "\n")
+            fichero.write("Fitness: " + str(fitness) + "\n")
+            fichero.write("\n")
+            contador+=1
+        fichero.write("\n\n\n")
 
-fichero.close()
+    fichero.close()
