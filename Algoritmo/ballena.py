d037ea04aa581bf1bcd7093b8807db41df7b0d82
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-05-18 10:25:52 +0200

    Versión 4

--- Algoritmo/ballena.py
+++ Algoritmo/ballena.py
@@ -6,7 +6,7 @@ TOLERANCIA = 0.01
 
 #Devuelve todas las funciones de la ballena implementadas
 def getFuncionesBallena():
-    return [Ballena1,Ballena2,Ballena3]
+    return [Ballena1,Ballena2,Ballena3,Ballena4]
 
 #Semilla aleatoria
 def setSeed(seed):
@@ -22,7 +22,7 @@ def generaPoblacionInicial(inf,sup,dimension,nBallenas=NUM_BALLENAS):
 def busquedaLocal(f_obj,inf,sup,dimension,solucion,max_evals):
     evals = 1
     score = f_obj(solucion)
-    sol_local = solucion
+    sol_local = np.copy(solucion)
     while evals<max_evals:
         vecino = sol_local+np.random.uniform(-0.1,0.1,dimension)
         vecino[vecino<inf] = inf
@@ -33,6 +33,41 @@ def busquedaLocal(f_obj,inf,sup,dimension,solucion,max_evals):
             sol_local = vecino
         evals+=1
     return sol_local,score
+
+def evolucionDiferencial(f_obj,poblacion,inf,sup,fitness):
+    nBallenas = len(poblacion)
+    n = np.array([])
+    #Genero los vectores de ruido
+    for i in range(nBallenas):
+        sample = np.arange(nBallenas)
+        np.random.shuffle(sample)
+        sample = sample[:3]
+        n = np.append(n,poblacion[sample[0]] + 0.5*(poblacion[sample[1]]-poblacion[sample[2]]))
+    n = n.reshape(nBallenas,len(poblacion[0]))
+
+    #Genero la recombinacion
+    t = np.array([])
+    for i in range(nBallenas):
+        if np.random.uniform(0,1)<(0.1/0.9):
+            t = np.append(t,n[i])
+        else:
+            t = np.append(t,poblacion[i])
+    t = t.reshape(nBallenas,len(poblacion[0]))
+
+    #Tomo como población recombinada los elementos según el fitness
+    poblacion_recombinada = np.array([])
+    fitness_recombinada = np.zeros(nBallenas)
+    for i in range(nBallenas):
+        fitness_t = f_obj(t[i])
+        if fitness_t<fitness[i]:
+            poblacion_recombinada = np.append(poblacion_recombinada,t[i])
+            fitness_recombinada[i] = fitness_t
+        else:
+            poblacion_recombinada = np.append(poblacion_recombinada,poblacion[i])
+            fitness_recombinada[i] = fitness[i]
+    poblacion_recombinada = poblacion_recombinada.reshape(nBallenas,len(poblacion[0]))
+    return poblacion_recombinada,fitness_recombinada
+
 
 ################################################################################
 ## Descripción: Función de la ballena primigenia. Es una traducción del       ##
@@ -268,7 +303,6 @@ def Ballena2(f_obj,inf,sup,dimension,nBallenas=NUM_BALLENAS):
     return lider_pos,lider_score
 
 
-
 ################################################################################
 ## Descripción: He cambiado el movimiento en espiral por una mutación         ##
 ## aleatoria del 20% de la población. Así mismo he metido que se hace la      ##
@@ -317,6 +351,137 @@ def Ballena3(f_obj,inf,sup,dimension,nBallenas=NUM_BALLENAS):
             for s in sample:
                 evaluaciones+=1000
                 posiciones[s],fitness[s] = busquedaLocal(f_obj,inf,sup,dimension,posiciones[s],1000)
+
+        for i in range(len(posiciones)):
+            #Devuelve a las ballenas que se han ido fuera del dominio al mismo
+            posiciones[i][posiciones[i]>sup] = sup
+            posiciones[i][posiciones[i]<inf] = inf
+
+            #Calcula el fitness de cada ballena
+            fitness[i] = f_obj(posiciones[i])
+
+            #Cambia al lider si la ballena es mejor
+            if fitness[i]<lider_score:
+                lider_score = np.copy(fitness[i])
+                lider_pos = np.copy(posiciones[i])
+
+        #Sumo nBallenas evaluaciones después de recalcular el fitness
+        evaluaciones+=nBallenas
+
+        #a se decrementa de forma lineal desde 2 hasta 0 en función de las iteraciones
+        a = 2-t*(2.0/max_iter)
+
+        #a2 se decrementa desde -1 a -2 de forma lineal
+        a2 = -1+t*(-1.0/max_iter)
+
+        for i in range(len(posiciones)):
+            #Numeros aleatorios entre 0 y 1
+            r1 = np.random.uniform(0,1)
+            r2 = np.random.uniform(0,1)
+
+            #Para calcular la siguiente posición de cada ballena
+            A = 2*a*r1-a
+            C = 2*r2
+
+            #Parámetros de la espiral logarítmica
+            b=1
+            l=(a2-1)*np.random.uniform(0,1)+1
+
+            #Número aleatorio para decidir si el movimiento es lineal o espiral
+            p = np.random.uniform(0,1)
+
+            if p<0.5:
+                #Si la norma es mayor que 1 entonces hacemos una aproximación lineal a una solución aleatoria.
+                if np.absolute(A)>=1:
+                    rand_lider_index = np.random.randint(0,nBallenas)
+                    X_rand = posiciones[rand_lider_index]
+                    D_X_rand = np.absolute(C*X_rand-posiciones[i])
+                    posiciones[i] = X_rand-A*D_X_rand
+                #Si la norma es menor que 1 hacemos una aproximación lineal a la mejor solución
+                else:
+                    D_lider = np.absolute(C*lider_pos-posiciones[i])
+                    posiciones[i] = lider_pos-A*D_lider
+            else:
+                sample = np.arange(nBallenas)
+                np.random.shuffle(sample)
+                slice = int(nBallenas*0.2)
+                sample = sample[:slice]
+                for s in sample:
+                    posiciones[s] = np.random.uniform(inf,sup,dimension)
+
+        t+=1
+
+    #Rehace los fitness y actualiza el lider
+    for i in range(len(posiciones)):
+        #Devuelve a las ballenas que se han ido fuera del dominio al mismo
+        posiciones[i][posiciones[i]>sup] = sup
+        posiciones[i][posiciones[i]<inf] = inf
+
+        #Calcula el fitness de cada ballena
+        fitness[i] = f_obj(posiciones[i])
+
+        #Cambia al lider si la ballena es mejor
+        if fitness[i]<lider_score:
+            lider_score = np.copy(fitness[i])
+            lider_pos = np.copy(posiciones[i])
+
+    lider_pos,lider_score = busquedaLocal(f_obj,inf,sup,dimension,lider_pos,1000)
+
+    return lider_pos,lider_score
+
+
+################################################################################
+## Descripción: Después de la búsqueda local he metido un esquema evolutivo   ##
+## como el de differential evolution.                                         ##
+################################################################################
+
+def Ballena4(f_obj,inf,sup,dimension,nBallenas=NUM_BALLENAS):
+    '''
+    @brief Devuelve un vector de tamaño dimension que contiene la solución para la función func_objetivo
+    @param f_obj Función objetivo que se pretende optimizar.
+    @param inf Límite inferior para cada valor del vector de soluciones.
+    @param sup Límite superior para cada valor del vector de soluciones.
+    @param dimension Dimensionalidad de la función.
+    @param min_max Valor booleano que indica si se minimiza o maximiza la función.
+    '''
+
+    #Inicializo el número de evaluaciones
+    max_evals = 10000*dimension
+    evaluaciones=0
+
+    #Inicializo la posición y score del líder
+    lider_pos = np.random.uniform(inf,sup,dimension)
+    lider_score = float('inf')
+
+    #Inicializa la posición de las ballenas
+    posiciones = generaPoblacionInicial(inf,sup,dimension)
+
+    #Contador de iteraciones
+    t=0
+    max_iter = (max_evals-1000)//nBallenas
+
+    #Valor real a
+    a = 2
+
+    #Fitness de cada ballena (inicialmente todo a ceros)
+    fitness = np.zeros(nBallenas)
+
+    #Bucle principal
+    while evaluaciones<max_evals-1000:
+
+        #Cada 1000 iteraciones hago una búsqueda local al 20% de la población
+        if t%1000==0:
+            #Tomamos las posiciones a las que hacemos la búsqueda local de forma aleatoria
+            sample = np.arange(nBallenas)
+            np.random.shuffle(sample)
+            slice = int(nBallenas*0.5)
+            sample = sample[:slice]
+            for s in sample:
+                evaluaciones+=1000
+                posiciones[s],fitness[s] = busquedaLocal(f_obj,inf,sup,dimension,posiciones[s],1000)
+
+            posiciones,fitness = evolucionDiferencial(f_obj,posiciones,inf,sup,fitness)
+
 
         for i in range(len(posiciones)):
             #Devuelve a las ballenas que se han ido fuera del dominio al mismo
