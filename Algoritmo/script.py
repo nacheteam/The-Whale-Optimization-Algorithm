0c7ccc1a0bb0d5cd3b3a3433987e470af94429f1
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-06-13 10:04:45 +0200

    Verbose

--- Algoritmo/script.py
+++ Algoritmo/script.py
@@ -10,6 +10,6 @@ arr = np.random.uniform(0,1,10)
 stdv = np.std(arr)
 print(stdv)
 
-es = cma.CMAEvolutionStrategy(arr,stdv)
+es = cma.CMAEvolutionStrategy(arr,stdv,{'seed': 123456789,'verb_disp':0})
 es.optimize(funciones_objetivo.getFuncion(1),verb_disp=0)
 print(es.result)
