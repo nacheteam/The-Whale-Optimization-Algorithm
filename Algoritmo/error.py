04fbe9133c54c67abef79dcbff91952139ee10eb
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-06-20 20:05:56 +0200

    Programa para obtener el error en cada versión

---
+++ Algoritmo/error.py
@@ -0,0 +1,16 @@
+'''
+Este script lee los ficheros de resultados y obtiene un fichero para cada uno
+con el error cometido en cada función.
+'''
+
+for i in range(6):
+    f = open("./v{}/resultados.txt".format(i+1))
+    g = open("./v{}/errores.txt".format(i+1))
+
+    for line in f:
+        num_f = 1
+        if "Fitness" in line:
+            fit = float(line.split("Fitness:")[1])
+            g.write(str(num_f) + "\t" + str(fit-100*num_f) + "\n")
+    g.close()
+    f.close()
