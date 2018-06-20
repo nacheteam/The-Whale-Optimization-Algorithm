8ce270dcd03584df768cd4cf771c597a06422ac8
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-06-20 20:11:49 +0200

    Sólo con la primera versión

--- Algoritmo/error.py
+++ Algoritmo/error.py
@@ -3,7 +3,7 @@ Este script lee los ficheros de resultados y obtiene un fichero para cada uno
 con el error cometido en cada función.
 '''
 
-for i in range(6):
+for i in range(1):
     f = open("./v{}/resultados.txt".format(i+1))
     g = open("./v{}/errores.txt".format(i+1))
 
